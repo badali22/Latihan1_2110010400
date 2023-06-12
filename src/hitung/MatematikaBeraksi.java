d2a0d47fa3205a7b6a0af08d91d5d4101bf0552f
badali22 <badaliamin22@gmail.com>
2023-06-12 21:31:13 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,5 +1,15 @@
 package hitung;
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika amin = new Matematika(0,0);
+        
+        System.out.println("Hasil Penjumlahan: "+amin.setPenjumlahan());
+        System.out.println("Hasil Pengurangan: "+amin.setPengurangan());
+        System.out.println("Hasil Perkalian: "+amin.setPerkalian());
+        System.out.println("Hasil Pembagian: "+amin.setPembagian());
+
+                
+    }
 }
